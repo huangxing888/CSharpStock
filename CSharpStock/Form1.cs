e06f5d991fe2eaae7724c0e14df0c8794e0bd446
153709265 <153709265@qq.com>
2018-11-20 10:36:10 +0800

    添加项目文件。

---
+++ CSharpStock/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace CSharpStock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IntPtr main = Process.GetProcessesByName("xiadan")[0].MainWindowHandle;
+            IntPtr child1 = GetDlgItem(main, 0x00000000);
+            IntPtr child2 = GetDlgItem(child1, 0x0000E900);
+            IntPtr child3 = GetDlgItem(child2, 0x0000E901);
+            IntPtr child4 = GetDlgItem(child3, 0x00000000);
+            IntPtr btn = GetDlgItem(child4,0x00008016);
+
+            SendMessage(btn, 0x00F5, 0, 0);
+        }
+
+        public delegate bool CallBack(int hwnd, int y);
+        [DllImport("user32.dll", EntryPoint = "FindWindowA", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", EntryPoint = "FindWindowEx", SetLastError = true)]
+        private static extern IntPtr FindWindowEx(IntPtr hwndParent, uint hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [DllImport("user32.dll", EntryPoint = "SendMessage", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int SendMessage(IntPtr hwnd, uint wMsg, int wParam, int lParam);
+
+        [DllImport("user32.dll", EntryPoint = "SetForegroundWindow", SetLastError = true)]
+        private static extern void SetForegroundWindow(IntPtr hwnd);
+        [DllImport("user32.dll")]
+        public static extern int EnumWindows(CallBack x, int y);
+        [DllImport("user32")]
+        public static extern int GetWindowText(int hwnd, StringBuilder lptrString, int nMaxCount);
+        [DllImport("user32.dll ", EntryPoint = "GetDlgItem")]
+        public static extern IntPtr GetDlgItem(IntPtr hDlg, int nIDDlgItem);
+    }
+}
