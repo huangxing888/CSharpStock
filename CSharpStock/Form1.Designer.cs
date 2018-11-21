7665b6884f9f70995e750b7826d168ba443ca5c0
153709265 <153709265@qq.com>
2018-11-21 23:59:13 +0800

    TreeView功能实现

--- CSharpStock/Form1.Designer.cs
+++ CSharpStock/Form1.Designer.cs
@@ -34,14 +34,14 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(85, 31);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(64, 25);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,10 +49,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(85, 82);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Location = new System.Drawing.Point(64, 66);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 26);
+            this.button2.Size = new System.Drawing.Size(75, 21);
             this.button2.TabIndex = 1;
             this.button2.Text = "买入";
             this.button2.UseVisualStyleBackColor = true;
@@ -60,10 +60,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(85, 126);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Location = new System.Drawing.Point(64, 101);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 26);
+            this.button3.Size = new System.Drawing.Size(75, 21);
             this.button3.TabIndex = 2;
             this.button3.Text = "卖出";
             this.button3.UseVisualStyleBackColor = true;
@@ -71,10 +71,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(85, 170);
-            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Location = new System.Drawing.Point(64, 136);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(100, 25);
+            this.button4.Size = new System.Drawing.Size(75, 20);
             this.button4.TabIndex = 3;
             this.button4.Text = "撤单";
             this.button4.UseVisualStyleBackColor = true;
@@ -82,10 +82,10 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(85, 212);
-            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Location = new System.Drawing.Point(64, 170);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(100, 25);
+            this.button5.Size = new System.Drawing.Size(75, 20);
             this.button5.TabIndex = 4;
             this.button5.Text = "持仓";
             this.button5.UseVisualStyleBackColor = true;
@@ -93,24 +93,35 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(451, 55);
+            this.richTextBox1.Location = new System.Drawing.Point(338, 44);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(363, 146);
+            this.richTextBox1.Size = new System.Drawing.Size(273, 118);
             this.richTextBox1.TabIndex = 5;
             this.richTextBox1.Text = "";
             // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(64, 196);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "刷新";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(913, 375);
+            this.ClientSize = new System.Drawing.Size(685, 300);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -125,6 +136,7 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- CSharpStock/Form1.cs
+++ CSharpStock/Form1.cs
@@ -141,10 +141,32 @@ namespace CSharpStock
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            main = Common.GetIntPtrByProcess("SwitchHosts");
-            Common.SetTitle(main, "hello");
-            addToDick(main);
-            MessageBox.Show(Common.GetTitle(main));
+            main = Common.GetIntPtrByProcess("xiadan");
+            IntPtr treePtr = Common.GetIntPtrByControlID(main, "00000000.0000E900.0000E900.00000081.000000C8.00000081");
+            //IntPtr root = GetRoot(treePtr);
+            //IntPtr nex = GetNextItem(treePtr, root);
+            //SelectNode(treePtr, nex);
+            //IntPtr child = GetFirstChildItem(treePtr, root);
+            //List<string> result = new List<string>();
+            //GetTreeViewText(treePtr, result);
+            //richTextBox1.Text = result.Count.ToString();
+            List<string> vOutput = new List<string>();
+            GetTreeViewText(treePtr, vOutput); // xxxx替换成相应treeview句柄。获得参考上贴FindWindow()的使用 
+            foreach (string vLine in vOutput)
+                Console.WriteLine(vLine);
+            IntPtr root = TreeView_GetRoot(treePtr);
+            SelectNode(treePtr, root);
+            //richTextBox1.AppendText(GetTreeItemText(treePtr, root));
+            IntPtr n = TreeView_GetRoot(treePtr);
+            while (n != IntPtr.Zero)
+            {
+                richTextBox1.AppendText(GetTreeItemText(treePtr, n) + "\n");
+                n = TreeView_GetNextSibling(treePtr, n);
+            }
+            //MessageBox.Show(GetItemText(treePtr, child));
+            //Common.SetTitle(main, "hello");
+            //addToDick(main);
+            //MessageBox.Show(Common.GetTitle(main));
             //IntPtr tree = FindWindowEx(main, 0, "SysTreeView32", "");
             //PostMessage(main, 256, Keys.F1, 2);
             //getStock();
@@ -180,6 +202,7 @@ namespace CSharpStock
             //SendMessage(save, WM_CLICK, IntPtr.Zero, "");
 
         }
+
 
         public void getStock()
         {
@@ -235,6 +258,232 @@ namespace CSharpStock
         private void button5_Click(object sender, EventArgs e)
         {
             User.PostMessage(main, 256, User.VK_F4, 2);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Common.Click(Common.GetIntPtrByControlID(main, "00000000.0000E900.0000E901.00000000.00008016"));
+        }
+        public const int TV_FIRST = 0x1100;
+        public const int TVM_GETCOUNT = TV_FIRST + 5;
+        public const int TVM_GETNEXTITEM = TV_FIRST + 10;
+        public const int TVM_GETITEMA = TV_FIRST + 12;
+        public const int TVM_GETITEMW = TV_FIRST + 62;
+        public const int TVM_SELECTITEM = 0x0000110b;
+        public const int TVGN_ROOT = 0x0000;
+        public const int TVGN_NEXT = 0x0001;
+        public const int TVGN_PREVIOUS = 0x0002;
+        public const int TVGN_PARENT = 0x0003;
+        public const int TVGN_CHILD = 0x0004;
+        public const int TVGN_FIRSTVISIBLE = 0x0005;
+        public const int TVGN_NEXTVISIBLE = 0x0006;
+        public const int TVGN_PREVIOUSVISIBLE = 0x0007;
+        public const int TVGN_DROPHILITE = 0x0008;
+        public const int TVGN_CARET = 0x0009;
+        public const int TVGN_LASTVISIBLE = 0x000A;
+
+        public const int TVIF_TEXT = 0x0001;
+        public const int TVIF_IMAGE = 0x0002;
+        public const int TVIF_PARAM = 0x0004;
+        public const int TVIF_STATE = 0x0008;
+        public const int TVIF_HANDLE = 0x0010;
+        public const int TVIF_SELECTEDIMAGE = 0x0020;
+        public const int TVIF_CHILDREN = 0x0040;
+        public const int TVIF_INTEGRAL = 0x0080;
+
+        [DllImport("User32.DLL")]
+        public static extern int SendMessage(IntPtr hWnd,
+            uint Msg, int wParam, int lParam);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(uint dwDesiredAccess,
+            bool bInheritHandle, uint dwProcessId);
+        public const uint MEM_COMMIT = 0x1000;
+        public const uint MEM_RELEASE = 0x8000;
+
+        public const uint MEM_RESERVE = 0x2000;
+        public const uint PAGE_READWRITE = 4;
+
+        public const uint PROCESS_VM_OPERATION = 0x0008;
+        public const uint PROCESS_VM_READ = 0x0010;
+        public const uint PROCESS_VM_WRITE = 0x0020;
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress,
+            uint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress,
+           uint dwSize, uint dwFreeType);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool CloseHandle(IntPtr handle);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress,
+           IntPtr lpBuffer, int nSize, ref uint vNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress,
+           IntPtr lpBuffer, int nSize, ref uint vNumberOfBytesRead);
+
+        [DllImport("user32.dll")]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd,
+            out uint dwProcessId);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct TVITEM
+        {
+            public int mask;
+            public IntPtr hItem;
+            public int state;
+            public int stateMask;
+            public IntPtr pszText;
+            public int cchTextMax;
+            public int iImage;
+            public int iSelectedImage;
+            public int cChildren;
+            public IntPtr lParam;
+            public IntPtr HTreeItem;
+        }
+
+        public static uint TreeView_GetCount(IntPtr hwnd)
+        {
+            return (uint)SendMessage(hwnd, TVM_GETCOUNT, 0, 0);
+        }
+
+        public static IntPtr TreeView_GetNextItem(IntPtr hwnd, IntPtr hitem, int code)
+        {
+            return (IntPtr)SendMessage(hwnd, TVM_GETNEXTITEM, code, (int)hitem);
+        }
+
+        public static IntPtr TreeView_GetRoot(IntPtr hwnd)
+        {
+            return TreeView_GetNextItem(hwnd, IntPtr.Zero, TVGN_ROOT);
+        }
+
+        public static IntPtr TreeView_GetChild(IntPtr hwnd, IntPtr hitem)
+        {
+            return TreeView_GetNextItem(hwnd, hitem, TVGN_CHILD);
+        }
+
+        public static IntPtr TreeView_GetNextSibling(IntPtr hwnd, IntPtr hitem)
+        {
+            return TreeView_GetNextItem(hwnd, hitem, TVGN_NEXT);
+        }
+
+        public static IntPtr TreeView_GetParent(IntPtr hwnd, IntPtr hitem)
+        {
+            return TreeView_GetNextItem(hwnd, hitem, TVGN_PARENT);
+        }
+
+        public static IntPtr TreeNodeGetNext(IntPtr AHandle, IntPtr ATreeItem)
+        {
+            if (AHandle == IntPtr.Zero || ATreeItem == IntPtr.Zero) return IntPtr.Zero;
+            IntPtr result = TreeView_GetChild(AHandle, ATreeItem);
+            if (result == IntPtr.Zero)
+                result = TreeView_GetNextSibling(AHandle, ATreeItem);
+
+            IntPtr vParentID = ATreeItem;
+            while (result == IntPtr.Zero && vParentID != IntPtr.Zero)
+            {
+                vParentID = TreeView_GetParent(AHandle, vParentID);
+                result = TreeView_GetNextSibling(AHandle, vParentID);
+            }
+            return result;
+        }
+
+        public static bool GetTreeViewText(IntPtr AHandle, List<string> AOutput)
+        {
+            if (AOutput == null) return false;
+            uint vProcessId;
+            GetWindowThreadProcessId(AHandle, out vProcessId);
+
+            IntPtr vProcess = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_READ |
+                PROCESS_VM_WRITE, false, vProcessId);
+            IntPtr vPointer = VirtualAllocEx(vProcess, IntPtr.Zero, 4096,
+                MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+            try
+            {
+                uint vItemCount = TreeView_GetCount(AHandle);
+                IntPtr vTreeItem = TreeView_GetRoot(AHandle);
+                Console.WriteLine(vItemCount);
+                for (int i = 0; i < vItemCount; i++)
+                {
+                    byte[] vBuffer = new byte[256];
+                    TVITEM[] vItem = new TVITEM[1];
+                    vItem[0] = new TVITEM();
+                    vItem[0].mask = TVIF_TEXT;
+                    vItem[0].hItem = vTreeItem;
+                    vItem[0].pszText = (IntPtr)((int)vPointer + Marshal.SizeOf(typeof(TVITEM)));
+                    vItem[0].cchTextMax = vBuffer.Length;
+                    uint vNumberOfBytesRead = 0;
+                    WriteProcessMemory(vProcess, vPointer,
+                        Marshal.UnsafeAddrOfPinnedArrayElement(vItem, 0),
+                        Marshal.SizeOf(typeof(TVITEM)), ref vNumberOfBytesRead);
+                    SendMessage(AHandle, TVM_GETITEMA, 0, (int)vPointer);
+                    ReadProcessMemory(vProcess,
+                        (IntPtr)((int)vPointer + Marshal.SizeOf(typeof(TVITEM))),
+                        Marshal.UnsafeAddrOfPinnedArrayElement(vBuffer, 0),
+                        vBuffer.Length, ref vNumberOfBytesRead);
+                    Console.WriteLine(Marshal.PtrToStringAnsi(
+                        Marshal.UnsafeAddrOfPinnedArrayElement(vBuffer, 0)));
+
+                    vTreeItem = TreeNodeGetNext(AHandle, vTreeItem);
+                }
+            }
+            finally
+            {
+                VirtualFreeEx(vProcess, vPointer, 0, MEM_RELEASE);
+                CloseHandle(vProcess);
+            }
+            return true;
+        }
+        
+        public static string GetTreeItemText(IntPtr treePtr,IntPtr vTreeItem)
+        {
+            uint vProcessId;
+            GetWindowThreadProcessId(treePtr, out vProcessId);
+            IntPtr vProcess = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_READ |
+                PROCESS_VM_WRITE, false, vProcessId);
+            IntPtr vPointer = VirtualAllocEx(vProcess, IntPtr.Zero, 4096,
+                MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+            byte[] vBuffer = new byte[256];
+            TVITEM[] vItem = new TVITEM[1];
+            vItem[0] = new TVITEM();
+            vItem[0].mask = TVIF_TEXT;
+            vItem[0].hItem = vTreeItem;
+            vItem[0].pszText = (IntPtr)((int)vPointer + Marshal.SizeOf(typeof(TVITEM)));
+            vItem[0].cchTextMax = vBuffer.Length;
+            uint vNumberOfBytesRead = 0;
+            WriteProcessMemory(vProcess, vPointer,
+                Marshal.UnsafeAddrOfPinnedArrayElement(vItem, 0),
+                Marshal.SizeOf(typeof(TVITEM)), ref vNumberOfBytesRead);
+            SendMessage(treePtr, TVM_GETITEMA, 0, (int)vPointer);
+            ReadProcessMemory(vProcess,
+                (IntPtr)((int)vPointer + Marshal.SizeOf(typeof(TVITEM))),
+                Marshal.UnsafeAddrOfPinnedArrayElement(vBuffer, 0),
+                vBuffer.Length, ref vNumberOfBytesRead);
+            return Marshal.PtrToStringAnsi(
+                Marshal.UnsafeAddrOfPinnedArrayElement(vBuffer, 0));
+        }
+        /// <summary> 
+        /// 选取TreeView指定项 
+        /// </summary> 
+        /// <param name= "TreeViewHwnd "> 树对象句柄 </param> 
+        /// <param name= "ItemHwnd "> 节点对象句柄 </param> 
+        /// <returns> 成功选中返回true 没找到返回false </returns> 
+        public static bool SelectNode(IntPtr TreeViewHwnd, IntPtr ItemHwnd)
+        {
+            int result = SendMessage(TreeViewHwnd, TVM_SELECTITEM, TVGN_CARET, (int)ItemHwnd);
+            if (result == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
